41273f2b3f5026a0b7f515f1e60bde853001e753
Samuel Défago <defagos@gmail.com>
2020-07-13 12:12:26 +0200

    Create project stucture (UIKit + SwiftUI)

---
+++ PlayLite/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  PlayLite
+//
+//  Created by Samuel Défago on 13.07.20.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.makeKeyAndVisible()
+            
+            window.rootViewController = UIHostingController(rootView: ContentView())
+            self.window = window
+        }
+    }
+}
