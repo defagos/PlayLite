48e06e7f02ec558381593327f6ba9b3f8519cbd9
Samuel Défago <defagos@gmail.com>
2020-07-14 13:13:28 +0200

    Display fake media object

--- PlayLite/MediaCell.swift
+++ PlayLite/MediaCell.swift
@@ -5,17 +5,54 @@
 //  Created by Samuel Défago on 13.07.20.
 //
 
+import FetchImage
 import SRGAppearance
 import SwiftUI
 
+struct Media {
+    let name: String
+    let imageUrl: URL
+    let date: Date
+}
+
+struct ImageView: View {
+    @ObservedObject var image: FetchImage
+    
+    init(url: URL) {
+        image = FetchImage(url: url)
+    }
+
+    public var body: some View {
+        ZStack {
+            Rectangle().fill(Color.black)
+            image.view?
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+        }
+        .animation(.default)
+        .onAppear(perform: image.fetch)
+        .onDisappear(perform: image.cancel)
+    }
+}
+
 struct MediaCell: View {
+    var media: Media
+    
+    static let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .none
+        return dateFormatter
+    }()
+    
     var body: some View {
-        HStack {
-            Image(systemName: "film")
+        VStack {
+            ImageView(url: media.imageUrl)
+                .aspectRatio(CGSize(width: 16, height: 9), contentMode: .fill)
             VStack {
-                Text("Title")
+                Text(media.name)
                     .font(.headline)
-                Text("Date")
+                Text(Self.dateFormatter.string(from: media.date))
                     .font(.subheadline)
             }
         }
@@ -24,8 +61,10 @@ struct MediaCell: View {
 
 struct MediaCell_Previews: PreviewProvider {
     static var previews: some View {
-        MediaCell()
+        let imageUrl = URL(string: "https://www.rts.ch/2020/07/13/17/11/10721864.image/16x9/scale/width/800")!
+        let media = Media(name: "Pardonnez-moi", imageUrl: imageUrl, date: Date())
+        MediaCell(media: media)
             .padding()
-            .previewLayout(.sizeThatFits)
+            .previewLayout(.fixed(width: 320, height: 220))
     }
 }
