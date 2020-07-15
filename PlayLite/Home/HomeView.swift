f01c43f14d3947a78d49725f77aaf319321dd56f
Samuel Défago <defagos@gmail.com>
2020-07-15 10:35:41 +0200

    Add more swimlanes

--- PlayLite/Home/HomeView.swift
+++ PlayLite/Home/HomeView.swift
@@ -9,22 +9,61 @@ import SRGDataProvider
 import SwiftUI
 
 class HomeModel: ObservableObject {
-    @Published private(set) var trending: [SRGMedia]?
-    private var request: SRGBaseRequest?
+    @Published private(set) var trendingMedias: [SRGMedia]?
+    @Published private(set) var mostSeenMedias: [SRGMedia]?
+    @Published private(set) var soonExpiringMedias: [SRGMedia]?
+    
+    private let requestQueue: SRGRequestQueue
     
     init() {
+        self.requestQueue = SRGRequestQueue.init()
         self.refresh()
     }
     
     private func refresh() {
-        request = SRGDataProvider.current?.tvTrendingMedias(for: .RTS, withLimit: 20, completionBlock: { (medias, _, _) in
-            self.trending = medias
+        let trendingMediasRequest = SRGDataProvider.current?.tvTrendingMedias(for: .RTS, withLimit: 20, completionBlock: { (medias, _, error) in
+            self.requestQueue.reportError(error)
+            self.trendingMedias = medias
         })
-        request?.resume()
+        if let trendingMediasRequest = trendingMediasRequest {
+            self.requestQueue.add(trendingMediasRequest, resume: true)
+        }
+        
+        let mostSeenMediasRequest = SRGDataProvider.current?.tvMostPopularMedias(for: .RTS, withCompletionBlock: { (medias, _, _, _, error) in
+            self.requestQueue.reportError(error)
+            self.mostSeenMedias = medias
+        }).withPageSize(20)
+        if let mostSeenMediasRequest = mostSeenMediasRequest {
+            self.requestQueue.add(mostSeenMediasRequest, resume: true)
+        }
+        
+        let soonExpiringMediasRequest = SRGDataProvider.current?.tvSoonExpiringMedias(for: .RTS, withCompletionBlock: { (medias, _, _, _, error) in
+            self.requestQueue.reportError(error)
+            self.soonExpiringMedias = medias
+        })
+        if let soonExpiringMediasRequest = soonExpiringMediasRequest {
+            self.requestQueue.add(soonExpiringMediasRequest, resume: true)
+        }
     }
     
     deinit {
-        self.request?.cancel()
+        self.requestQueue.cancel()
+    }
+}
+
+struct MediaSwimlane: View {
+    let medias: [SRGMedia]
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack(spacing: 10) {
+                ForEach(medias, id: \.uid) { media in
+                    MediaCell(media: media)
+                }
+            }
+            .padding(.leading, 10)
+            .padding(.trailing, 10)
+        }
     }
 }
 
@@ -33,28 +72,17 @@ struct HomeView: View {
     
     var body: some View {
         ZStack {
-            if let medias = model.trendingMedias {
-                VStack {
-                    ScrollView(.horizontal) {
-                        HStack(spacing: 10) {
-                            ForEach(medias, id: \.uid) { media in
-                                MediaCell(media: media)
-                            }
-                        }
-                        .padding(.leading, 10)
-                        .padding(.trailing, 10)
-                    }
-                    Spacer()
+            VStack {
+                if let medias = model.trendingMedias {
+                    MediaSwimlane(medias: medias)
                 }
-            }
-            else {
-                VStack {
-                    Image(systemName: "film")
-                        .resizable()
-                        .frame(width: 80, height: 80, alignment: .center)
-                    Text("No medias")
-                        .font(.title)
+                if let medias = model.mostSeenMedias {
+                    MediaSwimlane(medias: medias)
                 }
+                if let medias = model.soonExpiringMedias {
+                    MediaSwimlane(medias: medias)
+                }
+                Spacer()
             }
         }
     }
