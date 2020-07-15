dacb2cbe8444dc135e6abd935aac9de5da0479f5
Samuel Défago <defagos@gmail.com>
2020-07-15 16:39:04 +0200

    Use UIKit navigation to fix rotation issues

--- PlayLite/Application/SceneDelegate.swift
+++ PlayLite/Application/SceneDelegate.swift
@@ -16,7 +16,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             let window = UIWindow(windowScene: windowScene)
             window.makeKeyAndVisible()
             
-            window.rootViewController = UIHostingController(rootView: RootView())
+            let rootViewController = UIHostingController(rootView: HomeView())
+            window.rootViewController = UINavigationController(rootViewController: rootViewController)
             self.window = window
         }
     }
